3e765e79a35c55026ec39862f8dd9692d2a7af56
olia <oliush007@gmail.com>
2016-08-24 02:04:10 +0300

    add count of topics, posts, ets

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,32 @@ from django.utils import timezone
 from .models import Post, Category, Topic
 from django.http import HttpResponse
 
+
 def home_page(request):
     categories = Category.objects.all()
-    return render(request, 'blog/home_page.html', {'categories': categories})
+    categories_model = []
+    for category in categories:
+        topics = Topic.objects.filter(category_id=category.id).all()
+        posts_count = 0
+        for topic in topics:
+            posts = Post.objects.filter(topic_id=topic.id).all()
+            posts_count += len(posts)
+        len_topic = len(topics)
+        categories_model.append({'category': category, 'topics_count': len_topic,
+                                 'posts_count' : posts_count})
+    return render(request, 'blog/home_page.html', {'categories': categories_model})
 
 
 def category(request, category_id):
     topics = Topic.objects.filter(category_id=category_id)
-    # date_last_post = Post.objects.order_by(date__lte=timezone.now()).order_by('date')
-    return render(request, 'blog/category.html', {'topics': topics})
+    cat_f_top_model = []
+    for topic in topics:
+        posts = Post.objects.filter(topic_id=topic.id).all()
+        last_post = Post.objects.all()[Post.objects.count() - 1]
+        date_last_post = last_post.date
+        len_post_top = len(posts)
+        cat_f_top_model.append({'topic': topic, 'posts_count': len_post_top, 'datetime_last_post': date_last_post})
+    return render(request, 'blog/category.html', {'topics': cat_f_top_model})
 
 
 def topic(request, category_id, topic_id):
