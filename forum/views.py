04201896f561ff7a7462a55eb34eeb283554ad16
olia <oliush007@gmail.com>
2016-08-27 14:52:46 +0300

    revrited views on class based views

--- forum/views.py
+++ forum/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from .models import Post, Category, Topic
-from django.views.generic import View, ListView
+from django.views.generic import View, ListView, DetailView
+from django.utils import timezone
 
 
-class HomeView(View):
+class HomeView(ListView):
+    model = Category
+    template_name = 'forum/home_page.html'
 
-    def get(self, request):
-        categories = Category.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        # category_id = int(self.kwargs['category_id'])
         categories_model = []
-        for category in categories:
+        for category in Category.objects.all():
             topics = Topic.objects.filter(category_id=category.id).all()
             posts_count = 0
             for topic in topics:
@@ -17,26 +21,28 @@ class HomeView(View):
             len_topic = len(topics)
             categories_model.append({'category': category, 'topics_count': len_topic,
                                      'posts_count': posts_count})
-        return render(request, 'forum/home_page.html', {'categories': categories_model})
+        context['category_model'] = categories_model
+        print context
+        return context
 
-class CategoryView(View):
+
+class CategoryView(ListView):
     model = Topic
-    context_object_name = 'topics'
     template_name = 'forum/category.html'
 
-    def get(self, request, category_id):
+    def get_context_data(self, **kwargs):
+        context = super(CategoryView, self).get_context_data(**kwargs)
         category_id = int(self.kwargs['category_id'])
-
-        model = Topic.objects.filter(category_id=category_id)
         category_model = []
-        for topic in model:
-            posts = Post.objects.filter(topic_id=topic.id).all()
-            last_post = Post.objects.all()[Post.objects.count() - 1]
+        for topic in Topic.objects.filter(category_id=category_id):
+            posts = Post.objects.filter(topic_id=topic.id)
+            last_post = posts[posts.count() - 1]
             date_last_post = last_post.date
             len_post_top = len(posts)
             category_model.append({'topic': topic, 'posts_count': len_post_top, 'datetime_last_post': date_last_post})
+        context['category_model'] = category_model
+        return context
 
-        return render(request, 'forum/category.html', {'topics': category_model})
 
 class PostList(ListView):
     model = Post
